4f65493368916d56e10e82631d2b797bba1e5c54
Anttoni Koivu <anttoni.v.koivu+git@gmail.com>
2024-01-23 21:55:58 +0200

    Initial page design nearing completion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-	import { quintInOut } from "svelte/easing";
-	import { fade, draw } from "svelte/transition";
+	import { slide } from "svelte/transition";
+	import SocialIcons from "@rodneylab/svelte-social-icons"
 
 	let list: HTMLUListElement
 	let items: NodeListOf<HTMLLIElement>
@@ -87,32 +87,70 @@
 <svelte:window on:keydown={on_key_down}/>
 
 <div class="container mx-auto flex flex-col justify-center items-center gap-16">
-	<h1 class="font-bold text-5xl w-full text-center tracking-tight text-white">Anttoni Koivu</h1>
-
+	<h1 class="font-bold text-5xl text-center tracking-wider text-white">Anttoni Koivu</h1>
 	<nav>
-		<ul bind:this={list} class="flex flex-col gap-8 text-xl justify-center list-container">
+		<ul bind:this={list} class="flex flex-col gap-4 text-xl justify-center list-container">
 			<li>
 				<button>About Me</button>
 			</li>
-			{#if expanded[0]}
-				<div transition:fade={{duration: 300}} class="transition-div items-start text-white text-sm">Hi</div>
-			{/if}
-			<li>
-				<button>Projects</button>
-			</li>
-			{#if expanded[1]}
-				<div transition:fade={{duration: 500}} class="transition-div items-start text-white text-sm">
-					Hi
-				</div>
-			{/if}
+			<div class="wrapper-div">
+				{#if expanded[0]}
+					<div transition:slide={{duration: 1000}} class="transition-div items-start text-white text-lg">
+						<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit orci, pulvinar iaculis erat ut, placerat consectetur ligula. Quisque enim nulla, facilisis in sollicitudin et, vehicula mattis est. Praesent pulvinar ipsum eu consectetur consequat. Quisque a nibh dolor. Vivamus tortor libero, maximus vel ex quis, consectetur tempus dui. Cras tristique sapien diam, in blandit libero sagittis a. Curabitur cursus viverra magna non semper.
+	
+							Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum urna lectus, molestie eu sapien nec, fermentum suscipit est. Cras tincidunt a quam vel ultrices. Integer lacinia lacus in ex fermentum auctor. Duis urna felis, viverra id rhoncus eu, interdum nec elit. Vestibulum nulla dolor, pharetra vel nibh quis, ultricies hendrerit diam. Aenean ultricies lacus tortor, at vehicula leo vestibulum id. Aenean sit amet finibus ante. Quisque scelerisque elit ac turpis sodales, vitae convallis magna malesuada. Aliquam varius sem nisl, aliquam tincidunt ipsum scelerisque nec. Quisque luctus venenatis felis sit amet posuere. Proin ac magna sed ante dictum maximus. Curabitur non dolor nibh. </p>
+						</div>
+						{/if}
+					</div>
+					<li>
+						<button>Projects</button>
+					</li>
+					<div class="wrapper-div">
+						{#if expanded[1]}
+						<div transition:slide={{duration: 1000}} class="transition-div items-start text-white text-lg">
+							<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit orci, pulvinar iaculis erat ut, placerat consectetur ligula. Quisque enim nulla, facilisis in sollicitudin et, vehicula mattis est. Praesent pulvinar ipsum eu consectetur consequat. Quisque a nibh dolor. Vivamus tortor libero, maximus vel ex quis, consectetur tempus dui. Cras tristique sapien diam, in blandit libero sagittis a. Curabitur cursus viverra magna non semper.
+			
+								Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum urna lectus, molestie eu sapien nec, fermentum suscipit est. Cras tincidunt a quam vel ultrices. Integer lacinia lacus in ex fermentum auctor. Duis urna felis, viverra id rhoncus eu, interdum nec elit. Vestibulum nulla dolor, pharetra vel nibh quis, ultricies hendrerit diam. Aenean ultricies lacus tortor, at vehicula leo vestibulum id. Aenean sit amet finibus ante. Quisque scelerisque elit ac turpis sodales, vitae convallis magna malesuada. Aliquam varius sem nisl, aliquam tincidunt ipsum scelerisque nec. Quisque luctus venenatis felis sit amet posuere. Proin ac magna sed ante dictum maximus. Curabitur non dolor nibh. </p>
+					</div>
+				{/if}
+			</div>
 			<li>
 				<button>Contact</button>
 			</li>
-			{#if expanded[2]}
-				<div transition:fade={{duration: 500}} class="transition-div items-start text-white text-sm">
-					Hi and welcome to my website :)
+			<div class="wrapper-div">
+				{#if expanded[2]}
+				<div transition:slide={{duration: 1000}} class="transition-div items-start text-white text-lg">
+					<div class="icon-container">
+						<a href="https://www.linkedin.com/in/anttoni-koivu-a55070230/">
+							<SocialIcons id="icon" network="linkedin" width="32" height="32" fgColor="#ffffff"/>
+							<span class="icon-text">
+								Anttoni Koivu
+							</span>
+						</a>
+					</div>
+					<div class="icon-container">
+						<a href="https://github.com/Anttonii">
+							<SocialIcons id="icon" network="github" width="32" height="32" fgColor="#000000" bgColor="#ffffff"/>
+							<span class="icon-text">
+								Anttonii
+							</span>
+						</a>
+					</div>
+					<div class="icon-container">
+						<SocialIcons id="icon" network="discord" width="32" height="32" fgColor="#ffffff" />
+						<span class="icon-text">
+							Anttonii
+						</span>
+					</div>
+					<div class="icon-container">
+						<SocialIcons id="icon" network="telegram" width="32" height="32" fgColor="#ffffff" />
+						<span class="icon-text">
+							Anttoniii
+						</span>
+					</div>
 				</div>
-			{/if}
+				{/if}
+			</div>
 		</ul>
 	</nav>
 </div>
@@ -128,14 +166,23 @@
 		color: red;
 	}
 
-	.list-container {
-		flex-wrap: wrap;
-		max-width: 200px;
+	nav {
+		max-height: 90%;
+		width:600px;
 	}
 
 	.transition-div {
-		transition: all 0.3s ease;
-		width: 300px;
+		width: 600px;
+		max-height: 400px;
+		height: calc(100% + 20px);
+		box-sizing: border-box;
+		border: solid 1px white;
+		border-radius: 4px;
+	}
+
+	.wrapper-div {
+		margin: auto;
+		padding: auto;
 	}
 
 	:global(.triangle) {
@@ -160,6 +207,25 @@
 		clip-path: polygon(0 0, 0% 100%, 100% 50%);
 		z-index: 1;
 		position: relative;
+	}
+
+	div > p {
+		padding: 10px 20px 10px 20px;
+	}
+
+	.icon-container {
+		padding: 10px 20px 10px 20px;
+		position: relative;
+	}
+
+	.icon-text {
+		margin: 0px 0px 0px 12px;
+		position: absolute;
+		transform: translateY(10%);
+	}
+
+	a > span:hover {
+		text-decoration: underline;
 	}
 	
 </style>

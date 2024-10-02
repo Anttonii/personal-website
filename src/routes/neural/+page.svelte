7e8374277c2e4b638d0a203b394e6c3c001a3fd1
Anttoni Koivu <anttoni.v.koivu+git@gmail.com>
2024-10-02 14:25:10 +0300

    add: Instructions to the neural network page

--- src/routes/neural/+page.svelte
+++ src/routes/neural/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { afterUpdate, onMount } from 'svelte';
-	import { slide, fade } from 'svelte/transition';
+	import { fade, slide } from 'svelte/transition';
 
 	import { SyncLoader } from 'svelte-loading-spinners';
 
@@ -13,6 +13,7 @@
 	let gridValues: number[][] = new Array(28).fill(0).map(() => new Array(28).fill(0));
 	// the html element representing our canvas
 	let canvas: HTMLElement;
+	let expanderIcon: HTMLElement;
 
 	// box size refers to size of a single pixel in the grid thus a box takes boxSize ** 2 space in pixels
 	const gridSize: number = 28;
@@ -20,10 +21,7 @@
 	const black: string = '#000000';
 	const white: string = '#FFFFFF';
 
-	let mouseX = 0;
-	let mouseY = 0;
-
-	let currentlyDrawn: [number, number][] = [];
+	let instructionsExpanded = false;
 
 	afterUpdate(() => {
 		loaded = true;
@@ -47,7 +45,6 @@
 		}
 
 		isDrawing = false;
-		currentlyDrawn = [];
 	};
 
 	const handleMove = (event: MouseEvent) => {
@@ -56,27 +53,33 @@
 			return;
 		}
 
-		mouseX = event.pageX - canvas.getBoundingClientRect().x;
-		mouseY = event.pageY - canvas.getBoundingClientRect().y;
+		let mouseX = event.pageX - canvas.getBoundingClientRect().x;
+		let mouseY = event.pageY - canvas.getBoundingClientRect().y;
 
 		if (!isDrawing) return;
 
 		let gridX = Math.floor(mouseX / 16);
 		let gridY = Math.floor(mouseY / 16);
 
-		if (!currentlyDrawn.includes([gridX, gridY])) {
-			grid[gridX][gridY].style.background = white;
-			gridValues[gridX][gridY] = 1;
-			currentlyDrawn.push([gridX, gridY]);
-		}
+		grid[gridX][gridY].style.background = white;
+		gridValues[gridX][gridY] = 1;
 	};
 
 	const clearGrid = () => {
-		for (var i = 0; i < 28; i++) {
-			for (var j = 0; j < 28; j++) {
+		for (var i = 0; i < gridSize; i++) {
+			for (var j = 0; j < gridSize; j++) {
 				grid[i][j].style.background = black;
 				gridValues[i][j] = 0;
 			}
+		}
+	};
+
+	const toggleInstructions = () => {
+		instructionsExpanded = !instructionsExpanded;
+		if (!instructionsExpanded) {
+			expanderIcon.style.transform = 'rotate(180deg)';
+		} else {
+			expanderIcon.style.transform = 'rotate(0deg)';
 		}
 	};
 </script>
@@ -96,6 +99,35 @@
 				<a href="/">Anttoni Koivu</a>
 			</h2>
 			<h3 class="font-bold text-xl text-center tracking-wider text-white">Neural Network!</h3>
+			<div class="expander flex flex-col">
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<div
+					class="border border-white rounded text-white flex flex-row justify-between py-2 px-4"
+					on:click={toggleInstructions}
+				>
+					<h4 class="text-l">Instructions</h4>
+					<img
+						src="/images/triangle-32.png"
+						class="expander-icon inline-flex align-baseline ml-2"
+						width="16"
+						alt="expander triangle"
+						bind:this={expanderIcon}
+					/>
+				</div>
+				{#if instructionsExpanded}
+					<div class="expanded text-white pt-4 px-4" transition:slide={{ duration: 1000 }}>
+						<p class="text-sm">
+							Start by drawing an image of any number between 0 and 9. Press guess to see if the
+							neural network is correct at predicting what number you've drawn. If you want to start
+							over the drawing process, just press clear to clear the canvas.<br /><br />
+
+							Happy experimenting!
+						</p>
+					</div>
+				{/if}
+			</div>
+
 			<div class="flex flex-col justify-center gap-4">
 				<!-- svelte-ignore a11y-no-static-element-interactions -->
 				<div
@@ -144,5 +176,14 @@
 	.gridElem {
 		width: 16px;
 		height: 16px;
+	}
+
+	.expander {
+		width: 700px;
+	}
+
+	.expander-icon {
+		justify-self: right;
+		transform: rotate(180deg);
 	}
 </style>

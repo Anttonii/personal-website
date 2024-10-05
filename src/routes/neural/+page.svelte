6e9090bf002cf2c7aa292d851cabd47115db56a6
Anttoni Koivu <anttoni.v.koivu+git@gmail.com>
2024-10-05 16:57:56 +0300

    Initial backend integration

--- src/routes/neural/+page.svelte
+++ src/routes/neural/+page.svelte
@@ -14,12 +14,14 @@
 	// the html element representing our canvas
 	let canvas: HTMLElement;
 	let expanderIcon: HTMLElement;
+	let prediction: string;
 
 	// box size refers to size of a single pixel in the grid thus a box takes boxSize ** 2 space in pixels
 	const gridSize: number = 28;
 
 	const black: string = '#000000';
 	const white: string = '#FFFFFF';
+	const grey: string = '#5A5A5A';
 
 	let instructionsExpanded = false;
 
@@ -29,13 +31,56 @@
 
 	onMount(() => {});
 
-	const handleStart = () => {
+	const getPosition = (event: MouseEvent) => {
+		let mouseX = event.pageX - canvas.getBoundingClientRect().x;
+		let mouseY = event.pageY - canvas.getBoundingClientRect().y;
+		return [mouseX, mouseY];
+	};
+
+	const getPositionOnGrid = (event: MouseEvent) => {
+		let [mouseX, mouseY] = getPosition(event);
+		return [Math.floor(mouseX / 16), Math.floor(mouseY / 16)];
+	};
+
+	const inBoundCells = (position: [number, number]) => {
+		let numbers: number[][] = [];
+		if (position[0] > 0) {
+			numbers.push([position[0] - 1, position[1]]);
+		}
+		if (position[0] < gridSize) {
+			numbers.push([position[0] + 1, position[1]]);
+		}
+		if (position[1] > 0) {
+			numbers.push([position[0], position[1] - 1]);
+		}
+		if (position[1] < gridSize) {
+			numbers.push([position[0], position[1] + 1]);
+		}
+		return numbers;
+	};
+
+	const drawCell = (position: [number, number]) => {
+		grid[position[0]][position[1]].style.background = white;
+		gridValues[position[1]][position[0]] = 255;
+		let inBound = inBoundCells(position);
+		inBound.forEach((element) => {
+			if (gridValues[element[1]][element[0]] == 0) {
+				grid[element[0]][element[1]].style.background = grey;
+				gridValues[element[1]][element[0]] = 127;
+			}
+		});
+	};
+
+	const handleStart = (event: MouseEvent) => {
 		if (!canvas) {
 			console.log('Failure to load canvas!');
 			return;
 		}
 
 		isDrawing = true;
+
+		let [gridX, gridY] = getPositionOnGrid(event);
+		drawCell([gridX, gridY]);
 	};
 
 	const handleEnd = () => {
@@ -53,16 +98,10 @@
 			return;
 		}
 
-		let mouseX = event.pageX - canvas.getBoundingClientRect().x;
-		let mouseY = event.pageY - canvas.getBoundingClientRect().y;
-
 		if (!isDrawing) return;
 
-		let gridX = Math.floor(mouseX / 16);
-		let gridY = Math.floor(mouseY / 16);
-
-		grid[gridX][gridY].style.background = white;
-		gridValues[gridX][gridY] = 1;
+		let [gridX, gridY] = getPositionOnGrid(event);
+		drawCell([gridX, gridY]);
 	};
 
 	const clearGrid = () => {
@@ -81,6 +120,22 @@
 		} else {
 			expanderIcon.style.transform = 'rotate(0deg)';
 		}
+	};
+
+	const postGrid = async () => {
+		// Post the request to backend
+		const res = await fetch('http://localhost:5000/neural', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				gridValues
+			})
+		});
+
+		// Get the prediction result
+		const json = await res.json();
+		const result = JSON.stringify(json);
+		prediction = result;
 	};
 </script>
 
@@ -133,9 +188,9 @@
 				<div
 					class="canvas-grid flex flex-row"
 					bind:this={canvas}
-					on:mousemove={handleMove}
 					on:mousedown={handleStart}
 					on:mouseup={handleEnd}
+					on:mousemove={handleMove}
 				>
 					{#each { length: gridSize } as _, i}
 						<div class="row-{i}">
@@ -148,6 +203,7 @@
 				<div class="flex flex-row justify-center gap-4">
 					<button
 						class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+						on:click={postGrid}
 					>
 						Guess
 					</button>
@@ -159,6 +215,12 @@
 					</button>
 				</div>
 			</div>
+			{#if prediction}
+				<div class="flex flex-col justify-center gap-4 text-white">
+					<h2>The neural network predicted that you drew: {prediction}</h2>
+					<h3>Did it get it right? :)</h3>
+				</div>
+			{/if}
 		</div>
 	{/if}
 </div>
@@ -185,5 +247,6 @@
 	.expander-icon {
 		justify-self: right;
 		transform: rotate(180deg);
+		transition: all 1s;
 	}
 </style>

390cf412df1d9d4eeb19fc46edd5d48d9d7e873c
Anttoni Koivu <anttoni.v.koivu+git@gmail.com>
2024-11-24 21:09:05 +0200

    fix: Configuration changes

--- src/routes/basketball/[id]/+page.js
+++ src/routes/basketball/[id]/+page.js
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit'
 
 export async function load({ fetch, params }) {
-    let res = await fetch(`http://localhost:3000/api/players?player_id=${params.id}`, { method: 'GET' });
+    let res = await fetch(`/api/players?player_id=${params.id}`, { method: 'GET' });
     const data = res.json();
 
     if (res.ok) {
